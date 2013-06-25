81d8edf0ae217e64124c02182fc0473cf66bfe2a
Mihai Ciocan <yahim91@gmail.com>
2013-06-25 16:22:13 +0300

    implemented main channel connection

--- WebContent/main.js
+++ WebContent/main.js
@@ -1,7 +1,12 @@
 ;(function() {
-	var localStream = null,
-	peerConnection = null,
-	localVideo = null,
+	var localStream,
+	peerConnection,
+	localVideo,
+	userid,
+	mainChannel,
+	localChannel,
+	initiator,
+	channelConfig,
 	config = {
 		media : {
 			audio : true,
@@ -14,9 +19,38 @@
 			optional: [{"DtlsSrtpKeyAgreement": true}]
 		}
 	};
+	
 	function initialize() {
 		localVideo = document.getElementById("localVideo");
+		channelConfig = {
+				url: 'https://liquid-galaxy.firebaseio.com/main',
+				onmessage: function (data) {
+					if(data.userid === userid) {
+						return;
+					}
+					console.log('S -> C:' + JSON.stringify(data));
+					if (data.type === 'new_room') {
+						var startButton = document.getElementById("startButton");
+						startButton.parentNode.removeChild(startButton);
+						initiator = false;
+						joinRoom();
+					}
+					
+					if(data.type === 'new_participant' && initiator) {
+						handleNewParticipant(data.userid);
+					}
+				},
+				onopen: function () {
+					enableStartButton();
+				}
+		}
+		userid = createId();
 		getLocalUserMedia();
+		mainChannel = new Channel(channelConfig);
+	}
+	
+	function createId() {
+		return (Math.random() * new Date().getTime()).toString().toUpperCase().replace('.', '-');
 	}
 
 	// Get user media
@@ -24,16 +58,51 @@
 		getUserMedia(config.media, function(stream) {
 			attachMediaStream(localVideo, stream);
 			localStream = stream;
-			createPeerConnection();
 		}, function() {
 			throw new Error('Failed to get user media');
 		});
 	}
 	
+	function enableStartButton() {
+		var startButton = document.getElementById("startButton");
+		startButton.disabled = false;
+		startButton.onclick = function () {
+			this.parentNode.removeChild(this);
+			mainChannel.channel.onDisconnect().remove();
+			mainChannel.send({'userid': userid, 'type': 'new_room'});
+			initiator = true;
+		};
+	}
+	
+	function handleNewParticipant(userid) {
+		var userChannelConfig = {
+				url : 'https://liquid-galaxy.firebaseio.com/' + userid,
+				onmessage : processSignalingMessage,
+				onopen : function() {}
+			};
+		var userChannel = new Channel(userChannelConfig);
+		if (!peerConnection) {
+			createPeerConnection();
+		}
+	}
+
+	function joinRoom() {
+		// open own channel
+		localChannelConfig = {
+			url : 'https://liquid-galaxy.firebaseio.com/' + userid,
+			onmessage : processSignalingMessage,
+			onopen : function() {}
+		};
+		localChannel = new Firebase(localChannelConfig);
+		// send request
+		mainChannel.send({'userid': userid, 'type': 'new_participant'});
+	}
 
 	function createPeerConnection() {
 		peerConnection = new RTCPeerConnection(config.peerConnectionConfig, config.peerConnectionConstraints);
-		peerConnection.addStream(localStream);
+		if (localStream) {
+			peerConnection.addStream(localStream);
+		}
 		peerConnection.onicecandidate = onIceCandidate;
 		peerConnection.onaddstream = onRemoteStreamAdded;
 	}
@@ -45,29 +114,27 @@
 			console.log("End of candidates");
 		}
 	}
-	
+
 	function onRemoteStreamAdded(stream) {
-		
+
 	}
-	
-	function onChannelMessage(message) {
-		processSignalingMessage(message);
-	}
-	
-	function processSignalingMessage(message) {
-		var msg = JSON.parse(message);
+
+	function processSignalingMessage(msg) {
 		if (msg.type == 'offer') {
-	          peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
-	          answer();
-	          console.log("Set remote description.");
-	      } else if (msg.type === 'answer') {
-	        peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
-	      } else if (msg.type === 'candidate') {
-	        var candidate = new RTCIceCandidate({sdpMLineIndex:msg.label, candidate:msg.candidate});
-	        peerConnection.addIceCandidate(candidate);
-	      } else if (msg.type === 'bye' && started) {
-	          onRemoteHangup();
-	      }
+			peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
+			answer();
+			console.log("Set remote description.");
+		} else if (msg.type === 'answer') {
+			peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
+		} else if (msg.type === 'candidate') {
+			var candidate = new RTCIceCandidate({
+				sdpMLineIndex : msg.label,
+				candidate : msg.candidate
+			});
+			peerConnection.addIceCandidate(candidate);
+		} else if (msg.type === 'bye' && started) {
+			onRemoteHangup();
+		}
 	}
 	
 	function answer() {
@@ -87,15 +154,12 @@
 		socket.send(msgString);
 	}
 	
-	function Channel() {
-		var url = 'https://liquid-galaxy.firebaseio.com',
-		channel = null;
-		
-		channel = new  Firebase(url);
-		channel.on("child_added", function(event) {
-			onChannelMessage(event.val());
+	function Channel(channelConfig) {
+		this.channel = new Firebase(channelConfig.url);
+		channelConfig.onopen();
+		this.channel.on("child_added", function(event) {
+			channelConfig.onmessage(event.val());
 		});
-		channel.onDisconnect.remove();
 		console.log('Channel opened');
 	}
 
