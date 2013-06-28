e1bc4c3ef9815d1bf16184f4069ab881ecbe2aa4
Mihai Ciocan <yahim91@gmail.com>
2013-06-28 14:23:45 +0300

    users are able to visualize all participants

--- WebContent/main.js
+++ WebContent/main.js
@@ -1,91 +1,158 @@
-;(function() {
-	var localStream,
-	localParticipant,
-	participants,
-	localVideo,
-	userid,
-	mainChannel,
-	initiator,
-	channelConfig,
-	config = {
+;
+(function() {
+	var localStream, localParticipant, selectedVideo, participants, userid, mainChannel, initiator, channelConfig, config = {
 		media : {
 			audio : true,
 			video : true
 		},
-		peerConnectionConfig: {
-			iceServers: [{"url" : "stun:stun.l.google.com:19302"}]
+		peerConnectionConfig : {
+			iceServers : [ {
+				"url" : "stun:stun.l.google.com:19302"
+			} ]
 		},
-		peerConnectionConstraints: {
-			optional: [{"DtlsSrtpKeyAgreement": true}]
+		peerConnectionConstraints : {
+			optional : [ {
+				"DtlsSrtpKeyAgreement" : true
+			} ]
 		}
 	};
-	
+
 	function initialize() {
 		participants = {};
-		localVideo = document.getElementById("localVideo");
+		selectedVideo = document.getElementById('selectedVideo');
 		channelConfig = {
-				url: 'https://liquid-galaxy.firebaseio.com/main',
-				onmessage: function (data) {
-					if(data.userid === userid) {
-						return;
-					}
-					console.log('S -> C:' + JSON.stringify(data));
-					if (data.type === 'new_room') {
-						var startButton = document.getElementById("startButton");
-						startButton.parentNode.removeChild(startButton);
-						initiator = false;
-						joinRoom();
-					}
-					
-					if(data.type === 'new_participant' && initiator) {
-						handleNewParticipant(data.userid);
-					}
-				},
-				onopen: function () {
-					enableStartButton();
+			url : 'https://liquid-galaxy.firebaseio.com/main',
+			onmessage : function(data) {
+				if (data.userid === userid) {
+					return;
 				}
+				console.log('S -> C:' + JSON.stringify(data));
+				if (data.type === 'new_room') {
+					var startButton = document.getElementById("startButton");
+					startButton.parentNode.removeChild(startButton);
+					initiator = false;
+					joinRoom();
+				}
+
+				if (data.type === 'new_participant' /* && initiator */) {
+					handleNewParticipant(data.userid);
+				}
+			},
+			onopen : function() {
+				enableStartButton();
+			}
 		}
 		userid = createId();
-		getLocalUserMedia();
 		mainChannel = new Channel(channelConfig);
 	}
-	
+
 	function createId() {
-		return (Math.random() * new Date().getTime()).toString().toUpperCase().replace('.', '-');
+		return (Math.random() * new Date().getTime()).toString().toUpperCase()
+				.replace('.', '-');
 	}
 
 	// Get user media
-	function getLocalUserMedia() {
+	function getLocalUserMedia(callback) {
 		getUserMedia(config.media, function(stream) {
-			attachMediaStream(localVideo, stream);
 			localStream = stream;
+			callback();
 		}, function() {
 			throw new Error('Failed to get user media');
 		});
 	}
-	
+
 	function enableStartButton() {
 		var startButton = document.getElementById("startButton");
 		startButton.disabled = false;
-		startButton.onclick = function () {
+		startButton.onclick = function() {
 			this.parentNode.removeChild(this);
 			mainChannel.channel.onDisconnect().remove();
-			mainChannel.send({'userid': userid, 'type': 'new_room'});
+			getLocalUserMedia(function() {
+				mainChannel.send({
+					'userid' : userid,
+					'type' : 'new_room'
+				});
+
+				var mediaElement = document.createElement('video');
+				mediaElement.src = webkitURL.createObjectURL(localStream);
+				mediaElement.style.width = "200px";
+				mediaElement.style.height = "150px";
+				mediaElement.autoplay = true;
+				mediaElement.controls = true;
+				mediaElement.muted = true;
+				mediaElement.play();
+				mediaElement.onclick = function() {
+					selectedVideo.src = webkitURL.createObjectURL(localStream);
+					selectedVideo.autoplay = true;
+					selectedVideo.controls = true;
+					selectedVideo.muted = true;
+					selectedVideo.play();
+				};
+				var remoteMediaStreams = document
+						.getElementById('remoteVideos');
+				remoteMediaStreams.appendChild(mediaElement);
+
+			});
 			initiator = true;
 		};
 	}
-	
-	function handleNewParticipant(userid) {
-		participants[userid] = new Participant(userid);
-		participants[userid].call();
+
+	function handleNewParticipant(remoteUserid) {
+		if (initiator) {
+			participants[remoteUserid] = new Participant(remoteUserid);
+			participants[remoteUserid].call();
+		} else if (!participants[remoteUserid]) {
+			if (localStream) {
+				participants[remoteUserid] = new Participant(
+						remoteUserid < userid ? remoteUserid + '+' + userid
+								: userid + '+' + remoteUserid);
+				if (remoteUserid < userid) {
+					participants[remoteUserid].call();
+				}
+			} else {
+				participants[remoteUserid]='waitingMedia';
+			}
+		}
 	}
 
 	function joinRoom() {
 		// create local participant
-		localParticipant = new Participant(userid);
-		mainChannel.send({'userid': userid, 'type': 'new_participant'});
+		getLocalUserMedia(function() {
+			localParticipant = new Participant(userid);
+			mainChannel.send({
+				'userid' : userid,
+				'type' : 'new_participant'
+			});
+			var mediaElement = document.createElement('video');
+			mediaElement.src = webkitURL.createObjectURL(localStream);
+			mediaElement.style.width = "200px";
+			mediaElement.style.height = "150px";
+			mediaElement.autoplay = true;
+			mediaElement.controls = true;
+			mediaElement.muted = true;
+			mediaElement.play();
+			mediaElement.onclick = function() {
+				selectedVideo.src = webkitURL.createObjectURL(localStream);
+				selectedVideo.autoplay = true;
+				selectedVideo.controls = true;
+				selectedVideo.muted = true;
+				selectedVideo.play();
+			};
+			var remoteMediaStreams = document.getElementById('remoteVideos');
+			remoteMediaStreams.appendChild(mediaElement);
+			for (user in participants) {
+				if (participants[user] === 'waitingMedia') {
+					participants[user] = new Participant(
+							user < userid ? user + '+' + userid
+									: userid + '+' + user);
+					if (user < userid) {
+						participants[user].call();
+					}
+				}
+			}
+		});
 	}
-	
+
 	function Channel(channelConfig) {
 		this.channel = new Firebase(channelConfig.url);
 		channelConfig.onopen();
@@ -95,16 +162,18 @@
 		console.log('Channel opened');
 	}
 
-
 	Channel.prototype.send = function(message) {
 		this.channel.push(message);
 	}
-	
+
 	function Participant(userid) {
 		var self = this;
 		this.userid = userid;
 		this.peerConnection = new RTCPeerConnection(
 				config.peerConnectionConfig, config.peerConnectionConstraints);
+		if (localStream) {
+			this.peerConnection.addStream(localStream);
+		}
 		this.peerConnection.onicecandidate = function(event) {
 			if (event.candidate) {
 				self.channel.send({
@@ -117,32 +186,57 @@
 				console.log("End of candidates.");
 			}
 		};
-		this.peerConnection.onstream = this.onRemoteStreamAdded;
-		this.channel = new Channel({
-			url : 'https://liquid-galaxy.firebaseio.com/' + userid,
-			onmessage: function (msg) {
-				if (msg.type == 'offer') {
-					self.peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
-					self.answer();
-					console.log("Set remote description.");
-				} else if (msg.type === 'answer') {
-					self.peerConnection.setRemoteDescription(new RTCSessionDescription(msg));
-				} else if (msg.type === 'candidate') {
-					var candidate = new RTCIceCandidate({
-						sdpMLineIndex : msg.label,
-						candidate : msg.candidate
-					});
-					self.peerConnection.addIceCandidate(candidate);
-				} else if (msg.type === 'bye' && started) {
-					//onRemoteHangup();
-				}
-			},
-			onopen: function () {}
-		});
+		this.peerConnection.onaddstream = this.onRemoteStreamAdded;
+		this.peerConnection.onnegotationneeded = this.negotiationNeeded;
+		this.channel = new Channel(
+				{
+					url : 'https://liquid-galaxy.firebaseio.com/' + userid,
+					onmessage : function(msg) {
+						if (msg.type == 'offer') {
+							self.peerConnection
+									.setRemoteDescription(new RTCSessionDescription(
+											msg));
+							self.answer();
+							console.log("Set remote description.");
+						} else if (msg.type === 'answer') {
+							self.peerConnection
+									.setRemoteDescription(new RTCSessionDescription(
+											msg));
+						} else if (msg.type === 'candidate') {
+							var candidate = new RTCIceCandidate({
+								sdpMLineIndex : msg.label,
+								candidate : msg.candidate
+							});
+							self.peerConnection.addIceCandidate(candidate);
+						} else if (msg.type === 'bye' && started) {
+							// onRemoteHangup();
+						}
+					},
+					onopen : function() {
+					}
+				});
+		this.channel.channel.onDisconnect().remove();
 	}
-	
-	Participant.prototype.onRemoteStreamAdded = function (stream) {
-		//TODO
+
+	Participant.prototype.onRemoteStreamAdded = function(event) {
+		console.log("remote stream added");
+		var mediaElement = document.createElement('video');
+		mediaElement.src = webkitURL.createObjectURL(event.stream);
+		mediaElement.style.width = "200px";
+		mediaElement.style.height = "150px";
+		mediaElement.autoplay = true;
+		mediaElement.controls = true;
+		mediaElement.play();
+		mediaElement.onclick = function() {
+			selectedVideo.src = webkitURL.createObjectURL(event.stream);
+			selectedVideo.autoplay = true;
+			selectedVideo.controls = true;
+			selectedVideo.muted = false;
+			selectedVideo.play();
+		};
+		var remoteMediaStreams = document.getElementById('remoteVideos');
+		remoteMediaStreams.appendChild(mediaElement);
+		mediaElement.onclick();
 	};
 
 	Participant.prototype.answer = function() {
