d731209e66b1b6d5ab079c07fde235a881d851bc
Mihai Ciocan <yahim91@gmail.com>
2013-07-12 15:03:08 +0300

    handled a user or a slave leave

--- WebContent/participant.js
+++ WebContent/participant.js
@@ -72,7 +72,7 @@ function Participant(participantConfig) {
 							},
 							onopen: function (participant) {
 								participant.peerConnection.addStream(self.participantConfig.stream);
-								participant.channel.onDisconnect().remove();
+								//participant.channel.channel.onDisconnect().remove();
 								participant.call();
 							}
 						});
